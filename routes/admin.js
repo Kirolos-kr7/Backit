5f9f7c05dfe3d8074b62805f0849a37ee8ca7041
Maro2807 <marotina28@gmail.com>
2022-05-02 17:14:16 +0200

    order sort

--- routes/admin.js
+++ routes/admin.js
@@ -31,6 +31,9 @@ adminRouter.get("/bids", authValidation, async (req, res) => {
   let user = res.locals.user;
   let { sortBy, dir } = req.query;
 
+  if (!sortBy) sortBy = "name";
+  if (!dir) dir = "asc";
+
   try {
     if (!user.isAdmin)
       return res.send({ message: "Access Denied!", ok: false });
@@ -64,6 +67,28 @@ adminRouter.get("/reports", authValidation, async (req, res) => {
       .find()
       .sort([[sortBy, dir]])
       .select("name email isAdmin");
+
+    res.send({ data: users, ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+adminRouter.get("/orders", authValidation, async (req, res) => {
+  let user = res.locals.user;
+  let { sortBy, dir } = req.query;
+
+  if (!sortBy) sortBy = "name";
+  if (!dir) dir = "asc";
+
+  try {
+    if (!user.isAdmin)
+      return res.send({ message: "Access Denied!", ok: false });
+
+    let users = await userModel
+      .find()
+      .sort([[sortBy, dir]])
+      .select("name email address isAdmin");
 
     res.send({ data: users, ok: true });
   } catch (err) {
